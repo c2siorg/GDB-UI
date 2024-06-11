5065efbef776a0b8f7bd6c3a920c32705c46b42a
Shubh942 <93862397+Shubh942@users.noreply.github.com>
2024-06-11 22:00:18 +0530

    changes in test file

--- gdbui_server/flask_test.py
+++ gdbui_server/flask_test.py
@@ -15,7 +15,7 @@ class TestGDBRoutes(TestCase):
 
         compile_payload = {
             "code": "#include <iostream>\nint main() { std::cout << 'Hello, World!'; return 0; }",
-            "name": "test_program",
+            "name": f"{self.temp_dir.name}/test_program",
         }
         self.client.post('/compile', data=json.dumps(compile_payload), content_type='application/json')
 
@@ -23,14 +23,11 @@ class TestGDBRoutes(TestCase):
         self.temp_dir.cleanup()
 
     def test_compile_code(self):
-        temp_dir = tempfile.TemporaryDirectory()
-        
         payload = {
             "code": "#include <iostream>\nint main() { std::cout << 'Hello, Universe!'; return 0; }",
-            "name": "test_program2",
+            "name": f"{self.temp_dir.name}/test_program2",
         }
         response = self.client.post('/compile', data=json.dumps(payload), content_type='application/json')
-        temp_dir.cleanup()
         
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.json['success'])
@@ -38,7 +35,7 @@ class TestGDBRoutes(TestCase):
     def test_gdb_command(self):
         gdb_payload = {
             "command": "info locals",
-            "name": "test_program"
+            "name": f"{self.temp_dir.name}/test_program"
         }
         response = self.client.post('/gdb_command', data=json.dumps(gdb_payload), content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -47,7 +44,7 @@ class TestGDBRoutes(TestCase):
     def test_set_breakpoint(self):
         breakpoint_payload = {
             "location": "main",
-            "name": "test_program"
+            "name": f"{self.temp_dir.name}/test_program"
         }
         response = self.client.post('/set_breakpoint', data=json.dumps(breakpoint_payload), content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -56,12 +53,12 @@ class TestGDBRoutes(TestCase):
     def test_info_breakpoints(self):
         breakpoint_payload = {
             "location": "main",
-            "name": "test_program"
+            "name": f"{self.temp_dir.name}/test_program"
         }
         self.client.post('/set_breakpoint', data=json.dumps(breakpoint_payload), content_type='application/json')
 
         info_payload = {
-            "name": "test_program"
+            "name": f"{self.temp_dir.name}/test_program"
         }
         response = self.client.post('/info_breakpoints', data=json.dumps(info_payload), content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -69,7 +66,7 @@ class TestGDBRoutes(TestCase):
 
     def test_stack_trace(self):
         stack_trace_payload = {
-            "name": "test_program"
+            "name": f"{self.temp_dir.name}/test_program"
         }
         response = self.client.post('/stack_trace', data=json.dumps(stack_trace_payload), content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -77,7 +74,7 @@ class TestGDBRoutes(TestCase):
 
     def test_threads(self):
         threads_payload = {
-            "name": "test_program"
+            "name": f"{self.temp_dir.name}/test_program"
         }
         response = self.client.post('/threads', data=json.dumps(threads_payload), content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -85,7 +82,7 @@ class TestGDBRoutes(TestCase):
 
     def test_get_registers(self):
         get_registers_payload = {
-            "name": "test_program"
+            "name": f"{self.temp_dir.name}/test_program"
         }
         response = self.client.post('/get_registers', data=json.dumps(get_registers_payload), content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -93,7 +90,7 @@ class TestGDBRoutes(TestCase):
 
     def test_get_locals(self):
         get_locals_payload = {
-            "name": "test_program"
+            "name": f"{self.temp_dir.name}/test_program"
         }
         response = self.client.post('/get_locals', data=json.dumps(get_locals_payload), content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -101,7 +98,7 @@ class TestGDBRoutes(TestCase):
 
     def test_run_program(self):
         run_payload = {
-            "name": "test_program"
+            "name": f"{self.temp_dir.name}/test_program"
         }
         response = self.client.post('/run', data=json.dumps(run_payload), content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -109,7 +106,7 @@ class TestGDBRoutes(TestCase):
 
     def test_memory_map(self):
         memory_map_payload = {
-            "name": "test_program"
+            "name": f"{self.temp_dir.name}/test_program"
         }
         response = self.client.post('/memory_map', data=json.dumps(memory_map_payload), content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -117,7 +114,7 @@ class TestGDBRoutes(TestCase):
 
     def test_continue_execution(self):
         continue_payload = {
-            "name": "test_program"
+            "name": f"{self.temp_dir.name}/test_program"
         }
         response = self.client.post('/continue', data=json.dumps(continue_payload), content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -125,7 +122,7 @@ class TestGDBRoutes(TestCase):
 
     def test_step_over(self):
         step_over_payload = {
-            "name": "test_program"
+            "name": f"{self.temp_dir.name}/test_program"
         }
         response = self.client.post('/step_over', data=json.dumps(step_over_payload), content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -133,7 +130,7 @@ class TestGDBRoutes(TestCase):
 
     def test_step_into(self):
         step_into_payload = {
-            "name": "test_program"
+            "name": f"{self.temp_dir.name}/test_program"
         }
         response = self.client.post('/step_into', data=json.dumps(step_into_payload), content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -141,7 +138,7 @@ class TestGDBRoutes(TestCase):
 
     def test_step_out(self):
         step_out_payload = {
-            "name": "test_program"
+            "name": f"{self.temp_dir.name}/test_program"
         }
         response = self.client.post('/step_out', data=json.dumps(step_out_payload), content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -150,7 +147,7 @@ class TestGDBRoutes(TestCase):
     def test_add_watchpoint(self):
         add_watchpoint_payload = {
             "variable": "var",
-            "name": "test_program"
+            "name": f"{self.temp_dir.name}/test_program"
         }
         response = self.client.post('/add_watchpoint', data=json.dumps(add_watchpoint_payload), content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -159,13 +156,13 @@ class TestGDBRoutes(TestCase):
     def test_delete_breakpoint(self):
         set_breakpoint_payload = {
             "location": "main",
-            "name": "test_program"
+            "name": f"{self.temp_dir.name}/test_program"
         }
         self.client.post('/set_breakpoint', data=json.dumps(set_breakpoint_payload), content_type='application/json')
 
         delete_breakpoint_payload = {
             "breakpoint_number": 1,
-            "name": "test_program"
+            "name": f"{self.temp_dir.name}/test_program"
         }
         response = self.client.post('/delete_breakpoint', data=json.dumps(delete_breakpoint_payload), content_type='application/json')
         self.assertEqual(response.status_code, 200)
