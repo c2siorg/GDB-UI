e375c05ce95dc58cdb26847834a294aaf8be784c
Shubh942 <93862397+Shubh942@users.noreply.github.com>
2024-06-29 19:13:31 +0530

    Revert "Added testcase for react file"
    This reverts commit c0de18458ff68ca49c78e190307f0407c1f41e2c.

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -4,9 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.js",
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
   },
 });
