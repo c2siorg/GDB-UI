408771b6e7c0c4f3f0c0750c8db0f07f831d798a
Shubh942 <93862397+Shubh942@users.noreply.github.com>
2024-06-30 15:50:26 +0530

    changes in vite.config

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -8,5 +8,8 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
+    test: {
+      include: ["src/**/*.{test,spec}.{js,ts}"],
+    },
   },
 });
