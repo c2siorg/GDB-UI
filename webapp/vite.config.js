721f741dd9db9ade6c9e92d0c949637e89f63692
Naman Jain <namanjain9271@gmail.com>
2025-03-10 18:00:18 +0530

    Update vite.config.js

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss() ],
   test: {
     globals: true,
     environment: 'jsdom',
